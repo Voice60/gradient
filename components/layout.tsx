4fcea94972b33fb32d8142a7278bd46338eeb10d
Nikolay Krasnov <krasnovkola2004z@gmail.com>
2021-08-13 06:40:21 +0500

    reworked gradient page

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,40 @@
+import { Drawer } from '@material-ui/core'
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import styles from '../styles/layout.module.scss'
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { useRouter } from 'next/dist/client/router';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import Image from 'next/image';
 
 const Layout: NextPage = ({ children }) => {
+  const router = useRouter()
+  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false)
+
+  const hideDrawer = (): void => setIsDrawerOpen(false)
+  const showDrawer = (): void => setIsDrawerOpen(true)
+
   return (
     <div className={styles.container}>
+      {router.pathname !== '/' && <IconButton onClick={showDrawer} size='medium' edge="start" className={styles.drawerBtn} color="inherit" aria-label="menu">
+        <MenuIcon />
+      </IconButton>}
+      <Drawer anchor='left' open={isDrawerOpen} onClose={hideDrawer}>
+        <List>
+          <ListItem  button onClick={() => router.push('/')} className={styles.icon}>
+            <ListItemIcon className={styles.icon}><Image width={32} height={32} src='/favicon.ico' alt='icon'></Image></ListItemIcon>
+          </ListItem>
+          <Divider />
+          <ListItem className={router.pathname === '/generate' ? styles.listItem_selected : ''} button onClick={() => router.push('/generate')} >
+            <ListItemText  primary='Генерация градиента' />
+          </ListItem>
+        </List>
+      </Drawer>
       {children}
       <footer className={styles.footer}>
         Created by Voice
--- pages/generate.tsx
+++ pages/generate.tsx
@@ -3,43 +3,70 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import Layout from '../components/layout'
+// import styles from '../styles/generate.module.scss'
 import styles from '../styles/generate.module.scss'
 import { useRef, useState } from 'react'
 import Button from '@material-ui/core/Button';
 import { createTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import { blue } from '@material-ui/core/colors'
+import CachedIcon from '@material-ui/icons/Cached';
+import { ButtonGroup } from '@material-ui/core'
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
 
 const Generate: NextPage = () => {
-  const [gradient, setGradient] = useState<string>('')
+  const [gradient, setGradient] = useState<string[]>([])
   const [isCopied, setIsCopied] = useState<boolean>(false)
+  const [isSaved, setIsSaved] = useState<boolean>(false)
   const text = useRef<HTMLHeadingElement>(null)
-  const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'f']
+  type hexLetter = string | number
+  const hexArray: hexLetter[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f']
 
   const theme = createTheme({
     palette: {
       primary: {
-        main: blue[400]
+        main: '#2196f3'
       },
     },
   })
 
   const genereteLetter = (): string => {
-    const n = Math.floor(Math.random() * 15)
+    const n = Math.floor(Math.random() * 16)
     return hexArray[n].toString()
   }
 
+  const getGradientProperty = (gradient: string[]): string => {
+    let grd: string[] = gradient.map(e => '#' + e)
+    return `linear-gradient(90deg, ${grd.join(', ')})`
+  }
+
+  const saveGradient = (): void => {
+    if (!isSaved && gradient[0]) {
+      debugger
+      let gradients: [string[]] = JSON.parse(localStorage.gradients ? localStorage.gradients : '[]')
+      localStorage.setItem('gradients', `${gradients.push(gradient) && JSON.stringify(gradients)}`)
+      setIsSaved(true)
+    }
+  }
+
   const copyGradient = (): void => {
-    navigator.clipboard.writeText(gradient)
+    navigator.clipboard.writeText('background: ' + getGradientProperty(gradient))
     setIsCopied(true)
   }
 
   const generateGradient = (): void => {
     setIsCopied(false)
-    let newGradient = ''
-    for (let i = 0; i < 12; i++) {
-      newGradient += genereteLetter()
+    setIsSaved(false)
+    let newGradient: string[] = []
+    for (let i = 0; i < 2; i++) {
+
+      let grd: string = ''
+      for (let k = 0; k < 6; k++) {
+        grd += genereteLetter()
+      }
+      newGradient.push(grd)
     }
-    setGradient(`linear-gradient(90deg, #${newGradient.slice(0, 6)}, #${newGradient.slice(-6)})`)
+    setGradient(newGradient)
   }
 
   return (
@@ -48,16 +75,25 @@ const Generate: NextPage = () => {
         <title>Generator</title>
       </Head>
       <div className={styles.wrapper}>
-        <div className={styles.gradient} style={{ background: gradient ? gradient : 'black' }}></div>
+        <div className={styles.gradient} style={{ background: gradient[0] ? getGradientProperty(gradient) : 'black' }}></div>
         <h1 className={styles.title}>Generate Your Gradient</h1>
         <h2 className={`${styles.message} ${isCopied ? styles.message_active : ''}`} >Gradient copied!</h2>
-        <h2 style={{ visibility: gradient ? 'visible' : 'hidden' }} onClick={copyGradient} className={styles.gradientProperty}>
-          {'background: ' + gradient + ' '} <Image width={16} height={16} src="/copyIcon.svg" alt="copyIcon" />
+        <h2 style={{ visibility: gradient[0] ? 'visible' : 'hidden' }} onClick={copyGradient} className={styles.gradientProperty}>
+          {'background: ' + getGradientProperty(gradient) + ' '} <Image width={16} height={16} src="/copyIcon.svg" alt="copyIcon" />
         </h2>
         <ThemeProvider theme={theme}>
-          <Button onClick={generateGradient} className={styles.generateBtn} variant="contained" color="primary">
-            generate
-          </Button>
+          <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+            <Button size="large" onClick={generateGradient} variant="contained" color="primary">
+              <CachedIcon />
+              &nbsp;generate
+            </Button>
+            <Button onClick={saveGradient}>
+              {isSaved
+                ? <BookmarkIcon />
+                : <BookmarkBorderIcon />}
+              &nbsp;save
+            </Button>
+          </ButtonGroup>
         </ThemeProvider>
       </div>
     </Layout>
